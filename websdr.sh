95977a531b8d31b5f0c40cdccca22d77404bb278
sinantie <sinantie@gmail.com>
2017-06-04 12:47:57 -0700

    changed url

--- websdr.sh
+++ websdr.sh
@@ -2,9 +2,9 @@
 
 RUNTIME=websdr_controller_edison.js
 BROWSER=chromium
-START_FREQ="${1:-198}"
-START_MODE="${2:-am}"
-URL=http://websdr.ewi.utwente.nl:8901/?tune=${START_FREQ}${START_MODE}
+START_FREQ="${2:-198}"
+START_MODE="${3:-am}"
+URL=${1}/?tune=${START_FREQ}${START_MODE}
 
 # Setup broweser in headless mode
 $BROWSER --headless --disable-gpu --remote-debugging-port=9222 ${URL}&
